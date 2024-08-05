8d90e3e86b91a0e52be67ff62025291629ab815f
Marcio Gouveia <marcio_gouveia@outlook.com>
2024-08-05 20:28:55 -0300

    init

---
+++ scripts/script.py
@@ -0,0 +1,13 @@
+import csv
+
+# Path to the CSV file
+csv_file_path = 'data/data.csv'
+
+# Read and print the contents of the CSV file
+with open(csv_file_path, 'r') as csvfile:
+    result = "Hello, world!"
+    reader = csv.reader(csvfile)    
+    for row in reader:
+        print(', '.join(row))  # Print each row as a comma-separated string
+        result += resultS
+    print(f"::set-output name=my_output::{result}")
