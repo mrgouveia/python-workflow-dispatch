4d0af581869ae0618d487d189e26e4b87813177d
Marcio Gouveia <marcio_gouveia@outlook.com>
2024-08-05 22:56:30 -0300

    init

--- scripts/script.py
+++ scripts/script.py
@@ -1,4 +1,5 @@
 import csv
+import workflow
 
 # Path to the CSV file
 csv_file_path = 'data/strings.csv'
@@ -8,6 +9,8 @@ with open(csv_file_path, 'r') as csvfile:
     result = "Hello, world!"
     reader = csv.reader(csvfile)    
     for row in reader:
-        print(', '.join(row))  # Print each row as a comma-separated string
+        print('call workflow')
+        workflow.trigger_workflow()
+        #print(', '.join(row))  # Print each row as a comma-separated string
         result += str(row)
     print(f"::set-output name=my_output::{result}")
